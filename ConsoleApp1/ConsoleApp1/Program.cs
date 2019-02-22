6bc18deb8f62735673bf6b6bbd30aed494a0aa90
Lukis543 <lukis543@gmail.com>
2019-02-22 13:37:11 +0100

    send

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -22,13 +22,15 @@ namespace Adoptions
 
         public string Color;
 
+        public string Age;
+
 
 
         public void Speak()
 
         {
 
-            Console.WriteLine("My name is " + this.Name + " and I am " + this.Color);
+            Console.WriteLine("My name is " + this.Name + " and I am " + this.Color + this.Age);
 
         }
 
@@ -46,8 +48,8 @@ namespace Adoptions
 
         {
 
-            this.Color = "Red";
-
+            this.Color = "Brown";
+            this.Age = " 5 months old";
             this.Speak();
 
         }
@@ -64,7 +66,8 @@ namespace Adoptions
 
         {
 
-            this.Color = "Blue";
+            this.Color = "Pink";
+            this.Age = " 1 year and 2 months old";
             this.Speak();
 
         }
@@ -81,8 +84,8 @@ namespace Adoptions
 
         {
 
-            this.Color = "Green";
-            
+            this.Color = "White";
+            this.Age = " 3 months old";
             this.Speak();
 
         }
@@ -104,27 +107,63 @@ namespace Adoptions
                 if (answer1 == "Dog")
                 {
                     Console.WriteLine("Oh okey you want a " + answer1 + " let's see what we have here");
-                    Start1:
+                Start1:
                     Console.WriteLine("This is what we have Corgi number 1; Labrador number 2; Husky number 3");
+                    Console.WriteLine("Type in the number of the dog that you want to check out");
                     string dog1 = Console.ReadLine();
                     if (dog1 == "1")
                     {
                         Dog a = new Red("Corgi");
+                        Console.WriteLine("If you want this dog type in + if you want to check another dog press -");
+                        string accept = Console.ReadLine();
+                        if (accept == "+")
+                        {
+                            Console.WriteLine("Have a good day with your new friend ;)");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            goto Start1;
+                        }
+                  
 
                     }
                     else if (dog1 == "2")
                     {
                         Dog b = new Blue("Labrador");
+                        Console.WriteLine("If you want this dog type in + if you want to check another dog press -");
+                        string accept = Console.ReadLine();
+                        if (accept == "+")
+                        {
+                            Console.WriteLine("Have a good day with your new friend ;)");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            goto Start1;
+                        }
 
                     }
                     else if (dog1 == "3")
                     {
                         Dog c = new Green("Husky");
-
+                        Console.WriteLine("If you want this dog type in + if you want to check another dog press -");
+                        string accept = Console.ReadLine();
+                        if (accept == "+")
+                        {
+                            Console.WriteLine("Have a good day with your new friend ;)");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            goto Start1;
+                        }
                     }
                     else
                     {
                         Console.WriteLine("Have you typed in the correct number?");
+                        Console.ReadLine();
+                        goto Start1;
                     }
 
                 }
